d1a97af9a11bab02463c4dd00ecfd31067aad047
FelixLamarche <felixlamarche28@gmail.com>
2024-11-27 20:58:34 -0500

    Problem 18

--- Problem18/main.cpp
+++ Problem18/main.cpp
@@ -16,14 +16,47 @@ typedef typename std::vector<std::vector<int>> Vec2DInt;
 
 static int Solve(const Vec2DInt& nbs)
 {
-	std::vector<int> sums(nbs.size(), 0);
+	std::vector<int> sums(nbs.size() + 1, 0);
 
+	for (size_t i = nbs.size(); i > 0; i--)
+	{
+		const std::vector<int>& row = nbs[i - 1];
+		
+		for (size_t j = 0; j < row.size(); j++)
+		{
+			sums[j] = std::max(sums[j], sums[j + 1]) + row[j];
+		}
+	}
 
-	return 0;
+	return sums[0];
 }
 
 int main()
 {
+	int T;
+	std::cin >> T;
 
+	for (int i = 0; i < T; i++)
+	{
+		int N;
+		std::cin >> N;
+
+		Vec2DInt nbs;
+		nbs.reserve(N);
+		for (size_t j = 0; j < N; j++)
+		{
+			std::vector<int> row;
+			row.reserve(j + 1);
+			for (size_t k = 0; k < j + 1; k++)
+			{
+				int nb;
+				std::cin >> nb;
+				row.push_back(nb);
+			}
+			nbs.push_back(row);
+		}
+
+		std::cout << Solve(nbs) << std::endl;
+	}
 	return 0;
 }
