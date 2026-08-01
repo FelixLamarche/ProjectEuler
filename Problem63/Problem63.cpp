3eaf2d629b59dfd48c40a1da91964e4cbbfe1e49
FelixLamarche <felixlamarche28@gmail.com>
2026-08-01 03:01:03 -0400

    Update Problem63.cpp

--- Problem63/Problem63.cpp
+++ Problem63/Problem63.cpp
@@ -46,8 +46,13 @@ static u64 Solve()
 			const uBigInt power = Power(nb, exponent);
 			const u32 digitsCount = DigitsCount(power);
 
+
 			if (digitsCount == exponent)
+			{
+
+				std::cout << nb.ToString() << "^" << exponent << " = " << power.ToString() << std::endl;
 				solCount++;
+			}
 			else if (digitsCount > exponent)
 				break;
 		}
